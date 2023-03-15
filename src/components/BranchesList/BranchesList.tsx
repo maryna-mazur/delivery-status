94cb509de5ce933ebd85e0ef0da44a0f8001e106
Maryna Mazur <marinemazur@gmail.com>
2023-03-15 19:46:19 +0200

    separated BranchesListForm

--- src/components/BranchesList/BranchesList.tsx
+++ src/components/BranchesList/BranchesList.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Branch } from "../../types/Branch";
 import { Settlement } from "../../types/Settlement";
 import { getSettlements, getWarehouses } from "../../utils/axios";
+import { BranchesListForm } from "../BranchesListForm";
 import { Loader } from "../Loader";
 
 import "./BranchesList.css";
 
 export const BranchesList = React.memo(() => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState<string>("");
   const [settlements, setSettlements] = useState<Settlement[]>([]);
   const [branches, setBranches] = useState<Branch[]>([]);
   const [settlementRef, setSettlementRef] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const createSettlementString = (settlement: Settlement) => {
+  const createSettlementString = useCallback((settlement: Settlement) => {
     const {
       SettlementTypeDescription,
       Description,
@@ -23,7 +24,7 @@ export const BranchesList = React.memo(() => {
     return SettlementTypeDescription !== "село"
       ? `${SettlementTypeDescription} ${Description}, ${AreaDescription}`
       : `${SettlementTypeDescription} ${Description}, ${RegionsDescription}, ${AreaDescription}`;
-  };
+  }, []);
 
   const handlerOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSettlementRef("");
@@ -67,29 +68,12 @@ export const BranchesList = React.memo(() => {
 
   return (
     <section className="branches-list">
-      <div className="branches-list-form">
-        <label htmlFor="settlements" className="branches-list-form__label">
-          Оберіть населений пункт:
-        </label>
-        <input
-          className="input branches-list-form__input"
-          type="text"
-          list="settlements"
-          value={query}
-          onChange={handlerOnChange}
-          placeholder="Ведіть назву населеного пункту"
-        />
-        <datalist id="settlements" className="branches-list-form__datalist">
-          {settlements &&
-            settlements.map((settlement) => (
-              <option
-                className="branches-list-form__option"
-                key={settlement.Ref}
-                value={createSettlementString(settlement)}
-              ></option>
-            ))}
-        </datalist>
-      </div>
+      <BranchesListForm
+        query={query}
+        handlerOnChange={handlerOnChange}
+        settlements={settlements}
+        createSettlementString={createSettlementString}
+      />
 
       {branches.length > 0 && !isLoading && (
         <>
---
+++ src/components/BranchesListForm/BranchesListForm.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FC } from "react";
+import { Settlement } from "../../types/Settlement";
+import './BranchesListForm.css';
+
+type BranchesListFormProps = {
+  query: string,
+  handlerOnChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  settlements: Settlement[],
+  createSettlementString: (settlement: Settlement) => string,
+}
+
+export const BranchesListForm: FC<BranchesListFormProps> = React.memo(({
+  query,
+  handlerOnChange,
+  settlements,
+  createSettlementString,
+}) => {
+  return (
+    <div className="branches-list-form">
+        <label htmlFor="settlements" className="branches-list-form__label">
+          Оберіть населений пункт:
+        </label>
+        <input
+          className="input branches-list-form__input"
+          type="text"
+          list="settlements"
+          value={query}
+          onChange={handlerOnChange}
+          placeholder="Ведіть назву населеного пункту"
+        />
+        <datalist id="settlements" className="branches-list-form__datalist">
+          {settlements &&
+            settlements.map((settlement) => (
+              <option
+                className="branches-list-form__option"
+                key={settlement.Ref}
+                value={createSettlementString(settlement)}
+              ></option>
+            ))}
+        </datalist>
+      </div>
+  );
+})
